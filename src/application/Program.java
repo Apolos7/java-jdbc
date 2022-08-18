9b99c8e9f777d62c592b7008ce4dc2b08cd08db5
José Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-08-18 15:08:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import mode.entities.Department;
+import mode.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Department obj = new Department(1, "books");
+		Seller seller = new Seller(21, "bob", "bob@example.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 	}
 
